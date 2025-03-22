dccbbe970fe5e6cafdd05eeb207443ab7afa1868
Fyy <121779725+fyydsz@users.noreply.github.com>
2025-03-22 20:23:32 +0700

    refactor App component structure and update routing logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Route, Routes, useLocation } from 'react-router-dom'
 import './App.css'
+import Home from './components/pages/Home'
+import NavBar from './components/navbar/NavBar'
+import NotFound from './components/pages/NotFound404'
+import { useEffect } from 'react'
+import About from './components/pages/About'
+import Support from './components/pages/Support'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const location = useLocation();
+  const isNotFound = location.pathname !== "/";
+  useEffect(() => {
+    if (location.hash) {
+      const id = location.hash.replace("#", ""); // Ambil ID dari hash
+      const element = document.getElementById(id);
+      if (element) {
+        setTimeout(() => {
+          element.scrollIntoView({ behavior: "smooth" });
+        }, 100); // Delay dikit biar elemen udah ada sebelum scroll
+      }
+    }
+  }, [location]); // Run setiap path berubah
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/support" element={<Support />}/>
+      </Routes>
+      {!isNotFound && <About />}
+    </div>
   )
 }
 
