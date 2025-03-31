c784c683a5ab7c40cab3f7fc535652042663411f
Fyy <121779725+fyydsz@users.noreply.github.com>
2025-04-01 00:03:45 +0700

    fix: comment out unused SidebarMenuItem in NavProjects component

--- src/components/layout/dashboard/nav-projects.tsx
+++ src/components/layout/dashboard/nav-projects.tsx
@@ -77,12 +77,12 @@ export function NavProjects({
             </DropdownMenu>
           </SidebarMenuItem>
         ))}
-        <SidebarMenuItem>
+        {/* <SidebarMenuItem>
           <SidebarMenuButton>
             <MoreHorizontal />
             <span>More</span>
           </SidebarMenuButton>
-        </SidebarMenuItem>
+        </SidebarMenuItem> */}
       </SidebarMenu>
     </SidebarGroup>
   )
