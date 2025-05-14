a45e63023af41034bd88eca2ed74a483bcc9582b
Fyy <121779725+fyydsz@users.noreply.github.com>
2025-05-14 21:04:09 +0700

    feat: implement new AppSidebar component with updated navigation structure

---
+++ src/components/layout/dashboard/main/app-sidebar.tsx
@@ -0,0 +1,192 @@
+import * as React from "react"
+import {
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  // Command,
+  // Frame,
+  GalleryVerticalEnd,
+  // Map,
+  // PieChart,
+  Settings2,
+  SquareTerminal,
+} from "lucide-react"
+
+import { NavMain } from "@/components/layout/dashboard/main/nav-main"
+// import { NavProjects } from "@/components/layout/dashboard/nav-projects"
+import { NavUser } from "@/components/layout/dashboard/nav-user"
+import { AppSwitcher } from "@/components/layout/dashboard/app-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  app: [
+    {
+      name: "Dashboard",
+      logo: GalleryVerticalEnd,
+      plan: "Main Dashboard",
+      url: "/dashboard",
+    },
+    {
+      name: "Docs Dashboard",
+      logo: AudioWaveform,
+      plan: "Docs Dashboard",
+      url: "/dashboard-docs",
+    },
+  ],
+  navMain: [
+    {
+      title: "Playground",
+      url: "#",
+      icon: SquareTerminal,
+      isActive: true,
+      items: [
+        {
+          title: "History",
+          url: "#",
+        },
+        {
+          title: "Starred",
+          url: "#",
+        },
+        {
+          title: "Settings",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Models",
+      url: "#",
+      icon: Bot,
+      items: [
+        {
+          title: "Genesis",
+          url: "#",
+        },
+        {
+          title: "Explorer",
+          url: "#",
+        },
+        {
+          title: "Quantum",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Documentation",
+      url: "#",
+      icon: BookOpen,
+      items: [
+        {
+          title: "Introduction",
+          url: "#",
+        },
+        {
+          title: "Get Started",
+          url: "#",
+        },
+        {
+          title: "Tutorials",
+          url: "#",
+        },
+        {
+          title: "Changelog",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings2,
+      items: [
+        {
+          title: "General",
+          url: "#",
+        },
+        {
+          title: "Team",
+          url: "#",
+        },
+        {
+          title: "Billing",
+          url: "#",
+        },
+        {
+          title: "Limits",
+          url: "#",
+        },
+      ],
+    },
+  ],
+  navDocs: [
+    {
+      title: "Configure",
+      url: "#",
+      icon: Settings2,
+      items: [
+        {
+          title: "Test",
+          url: "#",
+        },
+        {
+          title: "Test",
+          url: "#",
+        },
+        {
+          title: "Test",
+          url: "#",
+        },
+      ]
+    }
+  ]
+  // projects: [
+  //   {
+  //     name: "Design Engineering",
+  //     url: "#",
+  //     icon: Frame,
+  //   },
+  //   {
+  //     name: "Sales & Marketing",
+  //     url: "#",
+  //     icon: PieChart,
+  //   },
+  //   {
+  //     name: "Travel",
+  //     url: "#",
+  //     icon: Map,
+  //   },
+  // ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+
+  return (
+    <Sidebar className="select-none" collapsible="icon" {...props}>
+      <SidebarHeader>
+        <AppSwitcher app={data.app} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        {/* <NavProjects projects={data.projects} /> */}
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser/>
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
