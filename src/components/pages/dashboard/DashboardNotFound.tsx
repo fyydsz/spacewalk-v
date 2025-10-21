4618a09499195bef6362e3e2c04f4f6985d4c3cf
Fyy <121779725+fyydsz@users.noreply.github.com>
2025-10-21 13:18:45 +0700

    fix: update navigation path in DashboardNotFound component

--- src/components/pages/dashboard/DashboardNotFound.tsx
+++ src/components/pages/dashboard/DashboardNotFound.tsx
@@ -1,7 +1,6 @@
 import { SidebarInset } from "@/components/ui/sidebar";
 import { CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 import { useNavigate } from "react-router-dom";
 
 export default function DashboardNotFound() {
@@ -10,7 +9,7 @@ export default function DashboardNotFound() {
     <SidebarInset>
       <div className="flex flex-1 flex-col items-center justify-center h-screen gap-4">
         <CardTitle className="text-4xl font-bold">404 Not Found</CardTitle>
-        <Button variant="default" className={cn("bg-blue-500")} onClick={() => navigate("/dashboard")}>Back to dashboard?</Button>
+        <Button variant="default" onClick={() => navigate("/dashboard/main")}>Back to dashboard?</Button>
       </div>
     </SidebarInset>
   );
